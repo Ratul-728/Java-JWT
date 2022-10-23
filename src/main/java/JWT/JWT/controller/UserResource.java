5e2565a0945628dca161c40709f5eae896504935
Ratul-728 <s.m.alam@northsouth.edu>
2022-10-23 15:16:38 +0600

    include Controller

--- src/main/java/JWT/JWT/controller/UserResource.java
+++ src/main/java/JWT/JWT/controller/UserResource.java
@@ -1,10 +1,12 @@
 package JWT.JWT.controller;
 
+import JWT.JWT.domain.Role;
+import JWT.JWT.domain.User;
 import JWT.JWT.service.RoleService;
 import JWT.JWT.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -12,4 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
     private final UserService userService;
     private final RoleService roleService;
+
+    @GetMapping("/get-user")
+    public ResponseEntity<?> getUsers(){
+        return userService.getUser();
+    }
+    @PostMapping("/add-user")
+    public ResponseEntity<?> addUser(@RequestBody User user){
+        return userService.saveUser(user);
+    }
+    @GetMapping("/get-role")
+    public ResponseEntity<?> getRole(){
+        return roleService.getRole();
+    }
+    @PostMapping("/add-role")
+    public ResponseEntity<?> addRole(@RequestBody Role role){
+        return roleService.saveRole(role);
+    }
+
+    @PostMapping("/add-role-to-user")
+    public ResponseEntity<?> addRoleToUser(@RequestBody String username, String name){
+        return userService.addRoleToUser(username, name);
+    }
 }
